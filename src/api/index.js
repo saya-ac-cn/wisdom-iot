9115caa6dd06f52cdb972a5bf73a42730569a547
saya <saya@saya.ac.cn>
2022-01-15 22:44:59 +0800

    1、增加控制台接口

--- src/api/index.js
+++ src/api/index.js
@@ -9,29 +9,18 @@ import ajax from './ajax'
 // 后台api接口
 let backendAPI = '/backend';
 
-// 前台api接口
-let publicAPI = '/frontend';
-let frontendAPI = publicAPI + '/Pandora';
-
 // 登录接口
 export const requestLogin = params => ajax(`${backendAPI}/login`, params, 'POST');
 // 注销接口
 export const requestLogout = params => ajax(`${backendAPI}/logout`, params, 'POST');
-
-// 获取网关设备类型
-export const getIotGatewayType = params => ajax(`${backendAPI}/gateway/type`, params, 'GET');
-// 获取网关分页列表
-export const getIotGatewayPage = params => ajax(`${backendAPI}/gateway`, params, 'GET');
-// 添加网关
-export const addIotGateway = params => ajax(`${backendAPI}/gateway`, params, 'POST');
-// 修改网关
-export const editIotGateway = params => ajax(`${backendAPI}/gateway`, params, 'PUT');
-// 删除网关
-export const deleteIotGateway = params => ajax(`${backendAPI}/gateway/${params}`, {}, 'DELETE');
-// 获取网关详情
-export const getIotGatewayEntity = params => ajax(`${backendAPI}/gateway/${params}`, {}, 'GET');
-// 获取网关设备下拉选框
-export const getIotGatewayList = params => ajax(`${backendAPI}/gatewayList`, params, 'GET');
+// 查询最近的告警事件列表
+export const getLatestWarning = params => ajax(`${backendAPI}/chart/latestWarning${params}`, {}, 'GET');
+// 查询设备概览
+export const getClientOverview = params => ajax(`${backendAPI}/chart/clientOverview`, {}, 'GET');
+// 查询产品概览
+export const getProductOverview = params => ajax(`${backendAPI}/chart/productOverview`, {}, 'GET');
+// 查询近7天的数据上报情况
+export const getPre7DayCollect= params => ajax(`${backendAPI}/chart/pre7DayCollect`, {}, 'GET');
 
 // 获取设备分页列表
 export const getIotClientPage = params => ajax(`${backendAPI}/client`, params, 'GET');
@@ -41,12 +30,11 @@ export const addIotClient = params => ajax(`${backendAPI}/client`, params, 'POST
 export const editIotClient = params => ajax(`${backendAPI}/client`, params, 'PUT');
 // 删除设备
 export const deleteIotClient = params => ajax(`${backendAPI}/client`, params, 'DELETE');
-// 查看设备可用的序号
-export const getAvailableSerialNum = params => ajax(`${backendAPI}/client/serialNum/${params}`, {}, 'GET');
 // 获取设备下拉选框
 export const getClientSelectList = params => ajax(`${backendAPI}/client/select`, params, 'GET');
-// 获取产品下拉选框
-export const getIotProductList = params => ajax(`${backendAPI}/product/list`, params, 'GET');
+// 获取设备认证信息
+export const getIotIdentify = params => ajax(`${backendAPI}/identify/${params}`, {}, 'GET');
+
 
 // 调度计划
 export const getIotAppointmentPage = params => ajax(`${backendAPI}/appointment`, params, 'GET');
@@ -96,3 +84,5 @@ export const getStandardList = () => ajax(`${backendAPI}/standard`,{},'GET')
 export const getClientIotCollectionPage = params => ajax(`${backendAPI}/collection`, params, 'GET');
 // 查看历史执行过的指令
 export const getHistoryCommand = params => ajax(`${backendAPI}/command/history`, params, 'GET');
+// 查询指定设备最新的采集数据
+export const getClientLatestCollect = client => ajax(`${backendAPI}/collection/latest/${client}`, {}, 'GET');
