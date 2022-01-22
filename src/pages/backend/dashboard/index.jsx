46e54a3b7a1e46a39db34b085bc8d2ae42e43429
saya <saya@saya.ac.cn>
2022-01-22 22:09:33 +0800

    1、完成控制台页面告警列表

--- src/pages/backend/dashboard/index.jsx
+++ src/pages/backend/dashboard/index.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import {Row, Col, Card, Form, Input, Modal, Select, Table} from "antd";
-import {SyncOutlined} from '@ant-design/icons';
+import {Row, Col, Card, List, Skeleton, Modal, Select, Avatar} from "antd";
+import {SyncOutlined,WarningOutlined} from '@ant-design/icons';
 import DocumentTitle from 'react-document-title'
 import "./index.less"
+import {getLatestWarning} from "../../../api";
+import {openNotificationWithIcon} from "../../../utils/window";
 /*
  * 文件名：index.jsx
  * 作者：saya
@@ -13,12 +15,45 @@ import "./index.less"
 // 定义组件（ES6）
 class DashBoard extends Component {
 
-  constructor(props){
-    super(props)
+  state = {
+    // 最近的告警列表数据
+    latestWarningList: [],
+    latestWarningListLoading: false,
+  };
+
+  /**
+   * 获取最近的告警事件列表
+   * @returns {Promise<void>}
+   */
+  getLatestWarningList = async () => {
+    const _this = this;
+    // 在发请求前, 显示loading
+    _this.setState({latestWarningListLoading: true});
+    // 发异步ajax请求, 获取数据
+    const {msg, code, data} = await getLatestWarning(10);
+    // 在发请求前, 显示loading
+    _this.setState({latestWarningListLoading: false});
+    if (code === 0) {
+      // 在发请求前, 显示loading
+      _this.setState({latestWarningList: [{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":1,"clientId":null,"ruleId":null,"topic":"温度告警","content":"当前温度=21，超过阈值20","createTime":"2021-12-05 17:52:50","iotClient":{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":2,"productId":null,"identifyUuid":null,"name":"郁金香温湿度采集","lastLinkTime":"2021-09-21 08:53:06","enable":null,"remove":null,"belongUser":null,"createTime":null,"updateTime":null,"productName":null,"authenInfo":null,"startTime":null},"iotRule":{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":3,"name":"温度大于20摄氏度","productId":null,"abilityId":null,"symbol":null,"value1":null,"value2":null,"enable":null,"eventAttribute":null,"eventValue":null,"createTime":null,"updateTime":null,"abilityEntity":null,"startTime":null},"startTime":null}
+      ,{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":1,"clientId":null,"ruleId":null,"topic":"温度告警","content":"当前温度=21，超过阈值20","createTime":"2021-12-05 17:52:50","iotClient":{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":2,"productId":null,"identifyUuid":null,"name":"郁金香温湿度采集","lastLinkTime":"2021-09-21 08:53:06","enable":null,"remove":null,"belongUser":null,"createTime":null,"updateTime":null,"productName":null,"authenInfo":null,"startTime":null},"iotRule":{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":3,"name":"温度大于20摄氏度","productId":null,"abilityId":null,"symbol":null,"value1":null,"value2":null,"enable":null,"eventAttribute":null,"eventValue":null,"createTime":null,"updateTime":null,"abilityEntity":null,"startTime":null},"startTime":null},{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":1,"clientId":null,"ruleId":null,"topic":"温度告警","content":"当前温度=21，超过阈值20","createTime":"2021-12-05 17:52:50","iotClient":{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":2,"productId":null,"identifyUuid":null,"name":"郁金香温湿度采集","lastLinkTime":"2021-09-21 08:53:06","enable":null,"remove":null,"belongUser":null,"createTime":null,"updateTime":null,"productName":null,"authenInfo":null,"startTime":null},"iotRule":{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":3,"name":"温度大于20摄氏度","productId":null,"abilityId":null,"symbol":null,"value1":null,"value2":null,"enable":null,"eventAttribute":null,"eventValue":null,"createTime":null,"updateTime":null,"abilityEntity":null,"startTime":null},"startTime":null}
+      ,{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":1,"clientId":null,"ruleId":null,"topic":"温度告警","content":"当前温度=21，超过阈值20","createTime":"2021-12-05 17:52:50","iotClient":{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":2,"productId":null,"identifyUuid":null,"name":"郁金香温湿度采集","lastLinkTime":"2021-09-21 08:53:06","enable":null,"remove":null,"belongUser":null,"createTime":null,"updateTime":null,"productName":null,"authenInfo":null,"startTime":null},"iotRule":{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":3,"name":"温度大于20摄氏度","productId":null,"abilityId":null,"symbol":null,"value1":null,"value2":null,"enable":null,"eventAttribute":null,"eventValue":null,"createTime":null,"updateTime":null,"abilityEntity":null,"startTime":null},"startTime":null}
+      ,{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":1,"clientId":null,"ruleId":null,"topic":"温度告警","content":"当前温度=21，超过阈值20","createTime":"2021-12-05 17:52:50","iotClient":{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":2,"productId":null,"identifyUuid":null,"name":"郁金香温湿度采集","lastLinkTime":"2021-09-21 08:53:06","enable":null,"remove":null,"belongUser":null,"createTime":null,"updateTime":null,"productName":null,"authenInfo":null,"startTime":null},"iotRule":{"nowPage":null,"pageSize":null,"startLine":null,"endLine":null,"endTime":null,"id":3,"name":"温度大于20摄氏度","productId":null,"abilityId":null,"symbol":null,"value1":null,"value2":null,"enable":null,"eventAttribute":null,"eventValue":null,"createTime":null,"updateTime":null,"abilityEntity":null,"startTime":null},"startTime":null}]});
+    } else {
+      openNotificationWithIcon("error", "错误提示", msg);
+    }
+  }
+
+  /**
+   * 执行异步任务: 发异步ajax请求
+   */
+  componentDidMount() {
+    this.getLatestWarningList()
   }
 
 
   render() {
+    const {latestWarningListLoading,latestWarningList} = this.state;
     return (
       <DocumentTitle title='物联网智慧家庭·远程控制'>
         <section className="chart-v1">
@@ -26,10 +61,22 @@ class DashBoard extends Component {
             <Col className="panel-left">
               <Row gutter={[16, 16]}>
                 <Col span={11}>
-                  <Card title={<span className='operation-color'>告警事件</span>} hoverable={true} extra={<SyncOutlined className='operation-color'/>}>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                    <p>Card content</p>
+                  <Card title={<span className='operation-color'>告警事件</span>} hoverable={true} extra={<SyncOutlined onClick={this.getLatestWarningList} className='operation-color'/>}>
+                    <List
+                      className="demo-loadmore-list"
+                      loading={latestWarningListLoading}
+                      itemLayout="horizontal"
+                      dataSource={latestWarningList}
+                      renderItem={item => (
+                        <List.Item>
+                          <List.Item.Meta
+                            avatar={<Avatar icon={<WarningOutlined />} style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}/>}
+                            title={item.topic}
+                            description={`${!item.iotClient?'':item.iotClient.name}${item.content}[${item.createTime}]`}
+                          />
+                        </List.Item>
+                      )}
+                    />
                   </Card>
                 </Col>
                 <Col span={13}>
